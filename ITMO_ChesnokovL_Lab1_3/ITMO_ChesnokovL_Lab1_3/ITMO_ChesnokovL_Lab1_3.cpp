51d0511132c9147693156adf742082332d498266
cheslev <56174624+cheslev@users.noreply.github.com>
2022-06-19 17:29:49 +0300

    Lab1_3

---
+++ ITMO_ChesnokovL_Lab1_3/ITMO_ChesnokovL_Lab1_3/ITMO_ChesnokovL_Lab1_3.cpp
@@ -0,0 +1,32 @@
+﻿
+#include <Windows.h> 
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+    
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+    
+    cout.precision(2);
+
+    double a, b, c, p;
+    double perimeter;
+    double triangle_size;
+    cout << "a = ";
+    cin >> a;
+    cout << "\nb = ";
+    cin >> b;
+    cout << "\nc = ";
+    cin >> c;
+    perimeter = a + b + c;
+    p = perimeter / 2;
+    cout << "a + b + c = " << perimeter << endl;
+    triangle_size = sqrt(p * (p - a) * (p - b) * (p - c));
+
+    cout << "Периметр треугольника " << triangle_size;
+
+}
