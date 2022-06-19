e3e0c2431b85ce656acfa590c50e4aff96a9d851
cheslev <56174624+cheslev@users.noreply.github.com>
2022-06-19 17:21:50 +0300

    Lab1

---
+++ ITMO_ChesnokovL_Lab1/ITMO_ChesnokovL_Lab1/ITMO_ChesnokovL_Lab1.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	system("chcp 1251");
+	string name;
+	cout << "Введите свое имя";
+	double x;
+	double a, b;
+	cout << "\nВведите a и b:\n";
+	cin >> a;
+	cin >> name;
+	cin >> b;
+	x = a / b;
+	cout << "\nx = " << x << endl;
+	cout << "Привет, " << name << "!\n";
+	return 0;
+}
