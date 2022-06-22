7154bd42b290c4db12ee1a2752b762f8a452ddc8
cheslev <56174624+cheslev@users.noreply.github.com>
2022-06-22 12:35:53 +0300

    Lab_2_1,Lab2_2

---
+++ ITMO_ChesnokovL_Lab2_2_1/ITMO_ChesnokovL_Lab2_2_1/ITMO_ChesnokovL_Lab2_2_1.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << "Упражнение 2. Использование циклов при реализации алгоритмов\n";
+    cout << "Задание 1. Использование цикла с постусловием\n";
+
+    double x, x1, x2, y;
+
+    cout << "x1 = "; cin >> x1;
+    cout << "x2 = "; cin >> x2;
+
+    cout << "\tx\tsin(x)\n";
+
+    x = x1;
+
+    /*do
+    {
+        y = sin(x);
+        cout << "\t" << x << "\t" << y << endl;
+        x = x + 0.01;
+    }
+    while (x <= x2);*/
+
+    //Реализация задачи с помощью цикла с предусловием в рамках выполнения задания 3
+    while (x <= x2)
+    {
+        y = sin(x);
+        cout << "\t" << x << "\t" << y << endl;
+        x = x + 0.01;
+    }
+
+}
---
+++ ITMO_ChesnokovL_Lab2_2_2/ITMO_ChesnokovL_Lab2_2_2/ITMO_ChesnokovL_Lab2_2_2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << "Упражнение 2. Использование циклов при реализации алгоритмов\n";
+    cout << "Задание 2. Использование цикла с предусловием\n";
+
+    int a, b, temp;
+
+    cout << "a = "; cin >> a;
+    cout << "b = "; cin >> b;
+
+    /*while (a != b)
+    {
+        if (a > b)
+            a -= b; // аналогично выражению a = a - b
+        else
+            b -= a;
+    }
+    cout << "НОД = " << a << endl;*/
+
+    //Реализация задачи с помощью цикла с постусловием в рамках выполнения задания 3
+    do
+    {
+        if (a > b)
+            a -= b;
+        else
+            b -= a;
+
+        cout << "НОД = " << a << endl;
+    } while (a != b);
+}
