dd83bc463438c15b6a1f5ffe8f90dfcac6dde624
cheslev <56174624+cheslev@users.noreply.github.com>
2022-06-22 12:30:24 +0300

    Lab2_1_1, Lab2_1_2

---
+++ ITMO_ChesnokovL_Lab2_1/ITMO_ChesnokovL_Lab2_1/ITMO_ChesnokovL_Lab2_1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << "Упражнение 1. Реализация операторов выбора\n";
+    cout << "Задание 1. Определение принадлежности точки заданной фигуре\n\n";
+
+    float x, y;
+
+    cout << "Введите положение точки по оси Х: ";
+    cin >> x;
+    cout << "Введите положение точки по оси Y: ";
+    cin >> y;
+
+    if (x * x + y * y < 9 && y > 0)
+    {
+        cout << "внутри фигуры";
+    }
+    else if (x * x + y * y > 9 || y < 0)
+    {
+        cout << "вне фигуры";
+    }
+    else
+    {
+        cout << "на границе фигуры";
+    };
+}
---
+++ ITMO_ChesnokovL_Lab2_1_2/ITMO_ChesnokovL_Lab2_1_2/ITMO_ChesnokovL_Lab2_1_2.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    cout << "Упражнение 1. Реализация операторов выбора\n";
+    cout << "Задание 2. Использование оператора switch при реализации выбора\n\n";
+
+    char op;
+    cout << "Сделай свой выбор, собери авто свой мечты: ";
+    cin >> op;
+
+    /*switch (op)
+    {
+        case 'S':
+            cout << "Радио играть должно\n";
+            cout << "Колеса круглые\n";
+            cout << "Мощный двигатель\n";
+            break;
+        case 'V':
+            cout << "Кондиционер хочу\n";
+            cout << "Радио играть должно\n";
+            cout << "Колеса круглые\n";
+            cout << "Мощный двигатель\n";
+            break;
+        default:
+            cout << "Колеса круглые\n";
+            cout << "Мощный двигатель\n";
+    }*/
+
+    switch (op)
+    {
+    case 'V':
+        cout << "Кондиционер хочу\n";
+
+    case 'S':
+        cout << "Радио играть должно\n";
+    default:
+        cout << "Колеса круглые\n";
+        cout << "Мощный двигатель\n";
+    }
+
+}
